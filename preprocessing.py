a4d62323849d1734f5391277230fab79d9f52474
kien <nguyenduckienuet@gmail.com>
2024-10-10 15:07:07 +0700

    update

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,9 @@
 import os
 import mne
 from osl import preprocessing
-import matplotlib
-matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
-
+plt.show = lambda *args, **kwargs: None
 
 def search_fif_files(directory):
     fif_file_paths = []
@@ -15,21 +13,18 @@ def search_fif_files(directory):
                 fif_file_paths.append(os.path.join(root, file))
     return fif_file_paths
 
-def plot_meg(fif_path, selected_channels=None):
 
     raw = mne.io.read_raw_fif(fif_path, preload=True)
     print(raw.info)
 
-    # Tạo tên file từ fif_path
-    base_name = os.path.basename(fif_path)  # Lấy tên file từ đường dẫn
-    file_name, _ = os.path.splitext(base_name)  # Tách tên file và phần mở rộng
+    base_name = os.path.basename(fif_path)
+    file_name, _ = os.path.splitext(base_name)  
 
-    # Vẽ PSD chỉ cho các kênh đã chọn
     if selected_channels is not None:
-        picks = mne.pick_types(raw.info, meg='mag', eeg=False)  # Chọn các kênh MEG
-        selected_picks = picks[:selected_channels]  # Chọn một số kênh
+        picks = mne.pick_types(raw.info, meg='mag', eeg=False) 
+        selected_picks = picks[:selected_channels]  
     else:
-        selected_picks = None  # Nếu không chỉ định, vẽ tất cả
+        selected_picks = None 
 
     raw.plot_psd(fmin=0.1, fmax=100, tmin=0, tmax=None, picks=selected_picks, show=False)
     plt.savefig(f'{file_name}_psd.png')
@@ -37,15 +32,10 @@ def plot_meg(fif_path, selected_channels=None):
 
 
 
-input_file_paths = r"D:\1_Work\6_MGL_MS\meg_analysis\data\aamod_meg_maxfilt_00002"
-preproc_file_paths = r"D:\1_Work\6_MGL_MS\meg_analysis\data\preprocessed"
+input_file_paths = r"/media/avitech/MyPassport/Kien/MEG_data/aamod_meg_maxfilt_00002"
+preproc_file_paths = r"/media/avitech/MyPassport/Kien/MEG_data/meg_preproc"
 
 input_files = search_fif_files(input_file_paths)
-preproc_files = search_fif_files(preproc_file_paths)
-
-if preproc_files:
-    print("Preprocessed files already exist. Do you want to overwrite them? (y/n) ")
-    overwrite = True if input().lower() in ["y", "yes"] else False
 
 config = """
     preproc:
@@ -64,5 +54,5 @@ output = preprocessing.run_proc_batch(
     config,
     input_files,
     outdir=preproc_file_paths,
-    overwrite=overwrite,
+    overwrite=False,
     )
