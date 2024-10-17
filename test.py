5128ea16653a403e0e0dc1424f6ffeca47e185c4
kien <nguyenduckienuet@gmail.com>
2024-10-17 15:05:15 +0700

    update

--- test.py
+++ test.py
@@ -3,10 +3,18 @@
 import os
 # from dask.distributed import Client
 from osl import source_recon, utils
-from a_preprocessing import search_files
 import osl.source_recon as osl_sr
 osl_sr.setup_fsl(directory='/media/avitech/CODE/Kiennd/2_MGL_MS/fsl')
 
+
+def search_files(directory, type, sub_name=None):
+    file_paths = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(type):
+                if sub_name is None or any(name in file for name in sub_name):
+                    file_paths.append(os.path.join(root, file))
+    return file_paths
 
 # (0) define directories
 # setup_file = "M3_010_002_setup.py"
@@ -16,11 +24,11 @@ osl_sr.setup_fsl(directory='/media/avitech/CODE/Kiennd/2_MGL_MS/fsl')
 path_preproc = r"/media/avitech/MyPassport/Kien/MEG_data/1_meg_preproc"
 path_mri = r"/media/avitech/MyPassport/Kien/MEG_data/0_mri_test"
 
+sub_nums = ['sub-CC110033']
 dir_preproc = search_files(path_preproc, type = '.fif', sub_name=sub_nums)
 dir_smri = search_files(path_mri, type= "T1.nii", sub_name=sub_nums)    
 dir_coregis = r"/media/avitech/MyPassport/Kien/MEG_data/2_meg_coregis"
 
-sub_nums = ['sub-CC110033']
 
 # (1) prepare coregistration
 # Settings
