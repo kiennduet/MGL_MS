37e40e0d544a0689a0a46d57d5a5b71244d8d267
kien <nguyenduckienuet@gmail.com>
2024-12-10 16:13:18 +0700

    10/12

--- z_dis_plot.py
+++ z_dis_plot.py
@@ -5,9 +5,7 @@ from side_function import search_files, get_subject_list
 
 
 # Đường dẫn file TSV
-file_path = '/media/avitech/MyPassport/Kien/MEG_data/updated_participants.tsv'  # Thay bằng đường dẫn của bạn
-
-# Đọc file TSV
+file_path = '/media/avitech/MyPassport/Kien/MEG_data/updated_participants.tsv' 
 data = pd.read_csv(file_path, sep='\t')
 
 path_preproc = r"/media/avitech/MyPassport/Kien/MEG_data/1_meg_preproc"
@@ -15,7 +13,6 @@ path_smri = r"/media/avitech/MyPassport/Kien/MEG_data/camcan/cc700/mri"
 
 sub_nums = get_subject_list(file_path=r"/media/avitech/My Passport/Kien/MEG_data/updated_participants.tsv", data_type=[0,1])
 print(f"Preprocessing {len(sub_nums)} subjects ...")
-
 
 dir_preproc, sub_nums = search_files(path_preproc, file_type = '.fif', sub_name=sub_nums, title="Preprocessing files")
 dir_smri, sub_nums= search_files(path_smri, file_type = "T1w.nii.gz", sub_name=sub_nums, title="MRI files")   
@@ -25,9 +22,7 @@ filtered_data = data[data['participant_id'].isin(sub_nums)]
 
 # Vẽ biểu đồ phân bố
 plt.figure(figsize=(10, 6))
-sns.histplot(filtered_data['age'], bins=25, kde=True, color='red', label='Age Distribution', line_kws={'color': 'blue'})
-
-# Thêm thông tin vào biểu đồ
+sns.histplot(filtered_data['age'], bins=25, kde=True, color='red', label='Age Distribution')
 plt.title(f'Age Distribution of {len(sub_nums)} Subjects', fontsize=16)
 plt.xlabel('Age', fontsize=14)
 plt.ylabel('Frequency', fontsize=14)
